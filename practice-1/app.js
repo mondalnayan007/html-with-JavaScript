b347368da546290c6825182692bfa465a9256e27
Nayan <n007kingkong@gmail.com>
2025-08-19 22:43:42 +0600

    website with JavaScript

---
+++ practice-1/app.js
@@ -0,0 +1,10 @@
+
+
+
+document.getElementById("update-btn").addEventListener("click",function(){
+    // console.log("button is clicked")
+    const updateTitle = document.getElementById("title");
+   
+
+    updateTitle.innerText = "My First Website Title With JavaScript";
+})
